54d35477d192e029853ead577a6a3a859c0bfd51
adeolu98 <hardeyholu98@gmail.com>
2021-07-16 20:31:13 +0100

    update

--- contracts/DivvydendPool.sol
+++ contracts/DivvydendPool.sol
@@ -8,19 +8,21 @@ import  './openzeppelin/contracts/security/ReentrancyGuard.sol';
 
    contract DivvydendPool is ReentrancyGuard {
  
-        address  public DivydendToken;
+        IERC20  public DivydendToken;
         address public admin;
-        address public rewardToken;
+        IERC20 public rewardToken;
         
         event depositReward (address from, uint amount);
         event  withdrawreward(address to, bool withdrawStatus);
         event adminSET(address newadmin);
-        event newRewardToken( address newrewardToken);
-        event newDivydendtoken(address newdivydendToken);
+        event newRewardToken( IERC20 newrewardToken);
+        event newDivydendtoken(IERC20 newdivydendToken);
         
         
         
-        constructor(address _DivydendToken, address _admin, address _rewardToken)  {
+        constructor(IERC20 _DivydendToken, address _admin, IERC20 _rewardToken)  {
+            //only 18 decimal tokens allowed, check token decimals before implementing them 
+            require(_DivydendToken.decimals() == 18 && _rewardToken.decimals() == 18, " decimals must be 18");
             DivydendToken = _DivydendToken;
             rewardToken = _rewardToken;
             admin = _admin;
@@ -35,11 +37,11 @@ import  './openzeppelin/contracts/security/ReentrancyGuard.sol';
         }
         
         // lets admin pay in rewards
-        function DepositReward( uint amount ) onlyAdmin public {
+        function DepositReward( uint amount ) onlyAdmin public nonReentrant{
             
-            require(IERC20(rewardToken).balanceOf(msg.sender) > 0 || IERC20(rewardToken).totalSupply() > amount, 'excess token amount');
+            require(rewardToken.balanceOf(msg.sender) > 0 || rewardToken.totalSupply() > amount, 'excess token amount');
            
-            IERC20(rewardToken).transferFrom( msg.sender, address(this), amount);
+            rewardToken.transferFrom( msg.sender, address(this), amount);
             
             
             emit depositReward(msg.sender, amount);
@@ -48,14 +50,16 @@ import  './openzeppelin/contracts/security/ReentrancyGuard.sol';
         //lets token holders withdraw reward tokens 
         
         function withdrawReward() nonReentrant public {
-            require(IERC20(DivydendToken).balanceOf(msg.sender) > 0 , 'zero divy token balance');
+            require(DivydendToken.balanceOf(msg.sender) >= 1*10**18 , 'divy token balance too low');
+            require(rewardToken.balanceOf(address(this)) >= 100*10*18, "reward token bal too low ");
             
-            uint DivydendTokenSupply = IERC20(DivydendToken).totalSupply();
-            uint msgsenderBal = IERC20(DivydendToken).balanceOf(msg.sender);
+            uint DivydendTokenSupply = DivydendToken.totalSupply();
+            uint msgsenderBal = DivydendToken.balanceOf(msg.sender);
             
-            uint msgSenderStakeInSupply = msgsenderBal/DivydendTokenSupply*100*10**18;
+            uint msgSenderStakeInSupply = msgsenderBal * 100000000000000000000 / DivydendTokenSupply; 
             
-            IERC20(rewardToken).transfer(msg.sender, msgSenderStakeInSupply);
+            uint reward = msgSenderStakeInSupply * rewardToken.balanceOf(address(this)) / 100000000000000000000 ;
+            rewardToken.transfer(msg.sender, reward);
             
             emit withdrawreward(msg.sender, true);
         }
@@ -68,14 +72,16 @@ import  './openzeppelin/contracts/security/ReentrancyGuard.sol';
     
     
     // allows for chamge of reward token address 
-    function changerewardToken( address newrewardToken) onlyAdmin public{
+    function changerewardToken( IERC20 newrewardToken) onlyAdmin public{
+        require(newrewardToken.decimals() == 18, "reward token decimal must be 18");
         rewardToken = newrewardToken;
         emit newRewardToken(rewardToken);
     }
     
     
     //allows for change of DivydendToken address
-     function changeDivydendToken( address newDivydendToken) onlyAdmin public{
+     function changeDivydendToken( IERC20 newDivydendToken) onlyAdmin public{
+        require(newDivydendToken.decimals() == 18, "token decimal must be 18");
         DivydendToken = newDivydendToken;
          emit newDivydendtoken(DivydendToken);
     }
